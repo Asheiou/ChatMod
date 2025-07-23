b8ee2fc168a5d4927c57925aaa52d3b65d6e11ef
Asheiou <31135263+Asheiou@users.noreply.github.com>
2025-07-23 02:41:42 +0100

    fix messagePrepped

--- src/main/kotlin/cymru/asheiou/chatmod/test/ChatTest.kt
+++ src/main/kotlin/cymru/asheiou/chatmod/test/ChatTest.kt
@@ -1,12 +1,13 @@
 package cymru.asheiou.chatmod.test
 
 import io.papermc.paper.event.player.AsyncChatEvent
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 
 abstract class ChatTest(val event: AsyncChatEvent, val permission: String? = null) {
   abstract fun test(): Boolean
 
-   val messagePrepped: String = run {
-    event.message().toString().replace(" ", "")
+  val messagePrepped: String = run {
+    PlainTextComponentSerializer.plainText().serialize(event.message()).replace(" ", "")
   }
 
   val hasPermission: Boolean = run {
